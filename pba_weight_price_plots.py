d2c844bef5be342c510113cc0c59edcdf9b6dbe9
Mike Prior-Jones <prior-jonesm@cardiff.ac.uk>
2025-07-23 16:03:06 +0100

    Changes price and weight plots to two-panel figure

--- pba_weight_price_plots.py
+++ pba_weight_price_plots.py
@@ -8,35 +8,36 @@ battery_data.sort_values(by='Capacity (Ah, 20hr rate)')
 
 # let's do weight first
 
-plt.figure(1)
+#plt.figure(1)
+fig, (ax1,ax2) = plt.subplots(2, sharex=True)
 
 # scatter points
-plt.scatter(battery_data['Capacity (Ah, 20hr rate)'], battery_data['Weight (kg)'])
+ax1.scatter(battery_data['Capacity (Ah, 20hr rate)'], battery_data['Weight (kg)'])
 
 # calculate linear trendline
 trendfit = np.polyfit(battery_data['Capacity (Ah, 20hr rate)'], battery_data['Weight (kg)'], 1)
 trendline = np.poly1d(trendfit)
 
 # plot trendline
-plt.plot(battery_data['Capacity (Ah, 20hr rate)'], trendline(battery_data['Capacity (Ah, 20hr rate)']))
+ax1.plot(battery_data['Capacity (Ah, 20hr rate)'], trendline(battery_data['Capacity (Ah, 20hr rate)']))
 
 # add titles
-plt.xlabel('Battery capacity (Ah, 20hr rate)')
-plt.ylabel('Battery mass, kg')
-plt.title('Lead-acid battery mass vs capacity')
+
+ax1.set_ylabel('Battery mass, kg')
+#plt.title('Lead-acid battery mass vs capacity')
 
 eqn_string = "%s" % trendline 
 
 # add equation
-plt.text(10, 35, ("y = %s" % eqn_string.strip()))
+ax1.text(10, 35, ("y = %s" % eqn_string.strip()))
 
-plt.savefig('battery_weight.png')
+#fig.savefig('battery_weight.png')
 
 # now price
 
-plt.figure(2)
+#plt.figure(2)
 # scatter points
-plt.scatter(battery_data['Capacity (Ah, 20hr rate)'], battery_data['Price euro'])
+ax2.scatter(battery_data['Capacity (Ah, 20hr rate)'], battery_data['Price euro'])
 
 # calculate linear trendline
 pricetrendfit = np.polyfit(battery_data['Capacity (Ah, 20hr rate)'], battery_data['Price euro'], 1)
@@ -44,17 +45,18 @@ pricetrendline = np.poly1d(pricetrendfit)
 
 
 # plot trendline
-plt.plot(battery_data['Capacity (Ah, 20hr rate)'], pricetrendline(battery_data['Capacity (Ah, 20hr rate)']))
+ax2.plot(battery_data['Capacity (Ah, 20hr rate)'], pricetrendline(battery_data['Capacity (Ah, 20hr rate)']))
 
 # add titles
-plt.xlabel('Battery capacity (Ah, 20hr rate)')
-plt.ylabel('Battery price, euros')
-plt.title('Lead-acid battery price vs capacity')
+#plt.xlabel('Battery capacity (Ah, 20hr rate)')
+ax2.set_xlabel('Battery capacity (Ah, 20hr rate)')
+ax2.set_ylabel('Battery price, euros')
+#plt.title('Lead-acid battery price vs capacity')
 
 eqn_string = "%s" % pricetrendline 
 
 
 # add equation
-plt.text(10, 300, ("y = %s" % eqn_string.strip()))
+ax2.text(10, 300, ("y = %s" % eqn_string.strip()))
 
-plt.savefig('battery_price.png')
+plt.savefig('battery_mass_price.png')
